cc55f1bb7c21d0ab643c67762603315ff5266be8
Rom <romulo.santos@travelperk.com>
2021-10-26 21:13:59 +0200

    sets up styled components and docker

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import styled from 'styled-components/macro'
+
+const Thing = styled.div`
+  background-color: red;
+  color: white;
+`
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Thing>...</Thing>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
